f9ee66b264e86418195154c9c56fa20f234969fc
Marc <marc@steema.com>
2019-02-27 11:31:56 +0100

    demo mods, paths to mdb. Advice about x64 x86 and MS OleDB Jet

--- Feature Demo/DataSource/DBDrillDown.cs
+++ Feature Demo/DataSource/DBDrillDown.cs
@@ -7,6 +7,12 @@ using System.Drawing;
 using System.Text;
 using System.Windows.Forms;
 
+/*
+* Special note.Access to the MS Access MDB demo is facilitated via the MS Jet 4.0 driver.Note that projects compiled
+* as x64 or 'any cpu' should have the corresponding 64 bit Jet driver in place.Otherwise, please set your project to
+* x84 in project options.
+*/
+
 namespace Steema.TeeChart.Samples
 {
   public partial class DBDrillDown : Steema.TeeChart.Samples.BaseForm
