1726fc93241bd695c55e6edda7debacfaaf867a0
Chris <chris@steema.com>
2020-06-05 15:11:24 +0200

    reset paths to TechProductsLtd.mdb

--- Feature Demo/Properties/Settings.Designer.cs
+++ Feature Demo/Properties/Settings.Designer.cs
@@ -12,7 +12,7 @@ namespace Steema.TeeChart.Samples.Properties {
     
     
     [global::System.Runtime.CompilerServices.CompilerGeneratedAttribute()]
-    [global::System.CodeDom.Compiler.GeneratedCodeAttribute("Microsoft.VisualStudio.Editors.SettingsDesigner.SettingsSingleFileGenerator", "15.7.0.0")]
+    [global::System.CodeDom.Compiler.GeneratedCodeAttribute("Microsoft.VisualStudio.Editors.SettingsDesigner.SettingsSingleFileGenerator", "16.6.0.0")]
     internal sealed partial class Settings : global::System.Configuration.ApplicationSettingsBase {
         
         private static Settings defaultInstance = ((Settings)(global::System.Configuration.ApplicationSettingsBase.Synchronized(new Settings())));
@@ -26,7 +26,7 @@ namespace Steema.TeeChart.Samples.Properties {
         [global::System.Configuration.ApplicationScopedSettingAttribute()]
         [global::System.Diagnostics.DebuggerNonUserCodeAttribute()]
         [global::System.Configuration.SpecialSettingAttribute(global::System.Configuration.SpecialSetting.ConnectionString)]
-        [global::System.Configuration.DefaultSettingValueAttribute("Provider=Microsoft.Jet.OLEDB.4.0;Data Source=\"..\\..\\TechProductsLtd.mdb\"")]
+        [global::System.Configuration.DefaultSettingValueAttribute("Provider=Microsoft.Jet.OLEDB.4.0;Data Source=\"..\\..\\..\\TechProductsLtd.mdb\"")]
         public string TechProductsLtdConnectionString {
             get {
                 return ((string)(this["TechProductsLtdConnectionString"]));
